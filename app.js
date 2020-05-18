1b6e4253985e82f8e7f770f36e404d3cffeadbf8
Alan Nguyen <tuan8101@gmail.com>
2020-05-18 00:40:36 -0600

    Implement Get strips

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+// GET Request
+app.get('/strips', (req, res, next) => {
+  db.all('SELECT * FROM Strip', (err, rows) => {
+    if (err) {
+      res.sendStatus(505);
+    } else {
+      res.send({ strips: rows });
+    }
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });

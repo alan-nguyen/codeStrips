f09259f0b882a9fb1c8195571abcc9369969e42f
Alan Nguyen <tuan8101@gmail.com>
2020-05-18 00:13:21 -0600

    Set up PORT and listen

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const app = express();
 
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+const PORT = process.env.PORT || 4001;
 
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 module.exports = app;

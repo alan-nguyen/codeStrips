883c0fd97c0dee330a2495c3c5acdf93a104941b
Alan Nguyen <tuan8101@gmail.com>
2020-05-18 00:40:02 -0600

    Import DB

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 const PORT = process.env.PORT || 4001;
 
+// Import DB
+var sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
